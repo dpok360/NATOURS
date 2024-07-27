6b19a294d0c5cb17022a66cdcb9f80d0ecef98c6
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2024-07-27 22:27:39 +0545

    added SIGTERM config

--- server.js
+++ server.js
@@ -40,3 +40,9 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED.Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated');
+  });
+});
