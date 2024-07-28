47c742eb78c591340255bfa95d0c5639d67b7735
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2024-07-28 13:21:32 +0545

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -24,6 +25,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+//implement CORS
+//Access-control-Allow_Origin *
+app.use(cors());
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
--- server.js
+++ server.js
@@ -7,10 +7,9 @@ process.on('uncaughtException', (err) => {
   console.log(err.name, err.message);
   process.exit(1);
 });
-
 dotenv.config({ path: './config.env' });
-const app = require('./app');
 
+const app = require('./app');
 const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD,
