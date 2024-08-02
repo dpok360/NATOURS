63f3a3451d79c031666b5d2ded3b6f46dd897f1c
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2024-08-03 01:34:40 +0545

    added new logout fn

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,9 +67,16 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, req, res);
 });
 
-exports.logout = (req, res) => {
+// exports.logout = (req, res) => {
+//   res.cookie('jwt', 'loggedout', {
+//     expires: new Date(Date.now() + 5 * 1000),
+//     httpOnly: true,
+//   });
+//   res.status(200).json({ status: 'success' });
+// };
+exports.logout = (req, res, next) => {
   res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
+    expires: new Date(0),
     httpOnly: true,
   });
   res.status(200).json({ status: 'success' });
