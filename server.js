3268dfef762a71cdf7e54436e38d316f6867b74e
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2024-11-25 23:17:46 +0545

    fix dotenv

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+dotenv.config();
 //UNCAUGHT EXCEPTION
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION!!!!!     SHUTTING DOWN........');
   console.log(err.name, err.message);
   process.exit(1);
 });
-dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
